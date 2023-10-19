25e5a24907d6d73b9ab35dc68ad0cd68535fd8fe
kt <cjtakhar@gmail.com>
2023-10-19 16:45:39 -0700

    update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaPaperPlane } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import "../styles/navigation.css";
 
 const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
-
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
@@ -18,13 +18,13 @@ const Navigation = () => {
             <FaPaperPlane />
           </Link>
         </h1>
-        <div className={`menu-toggle ${showMenu ? "active" : ""}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
+        <div
+          className={`menu-toggle ${showMenu ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <FaBars className="fabars" /> 
         </div>
-        <div className="nav-menu-container">
-        <ul className={`nav-menu ${showMenu ? "active" : ""}`}>
+        <ul className={`nav-menu ${showMenu ? "active" : "hidden"}`}>
           <li className="nav-item">
             <Link to="/portfolio">Portfolio</Link>
           </li>
@@ -35,7 +35,6 @@ const Navigation = () => {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-        </div>
       </div>
     </nav>
   );
