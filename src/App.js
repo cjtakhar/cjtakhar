c74d90794a035868dbf0c3df8cff037837025f1e
cjtakhar <cjtakhar@gmail.com>
2026-01-18 22:23:36 -0800

    navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Navigation from './components/Navigation';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Navigation from "./components/Navigation";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      < BrowserRouter >
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
